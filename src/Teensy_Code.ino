6974819b0268742b365b825370d3f7fb2b75228f
dmadariaga4 <84297351+dmadariaga4@users.noreply.github.com>
2021-10-30 11:44:17 -0300

    Added servo code

--- src/Teensy_Code.ino
+++ src/Teensy_Code.ino
@@ -1,17 +1,19 @@
 # include <Arduino.h>
+# include <Servo.h>
 
-int Pot_Input = 21;   
-int Motor_Output = 13; 
+Servo myservo;
+int Pot_Input = 21;    
 
 void setup() {
   pinMode(Pot_Input, INPUT);  
-  pinMode(Motor_Output, OUTPUT);
+  myservo.attach(13);
+  Serial.begin (9600);
+
 }
 
 void loop() {
   int val = analogRead(Pot_Input); 
   int angle = map(val,1,1023,0,270);
   Serial.println(angle);
+  myservo.write(angle);
 }
-
-// hello
