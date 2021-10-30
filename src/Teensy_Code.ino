dea9fbfde046ea5219e273f0b15723bb62b8ab0e
Jen Vibert <jennifer.vibert@dal.ca>
2021-10-30 11:13:40 -0300

    first commit

---
+++ src/Teensy_Code.ino
@@ -0,0 +1,14 @@
+
+int Pot_Input = 21;   
+int Motor_Output = 13; 
+
+void setup() {
+  pinMode(Pot_Input, INPUT);  
+  pinMode(Motor_Output, OUTPUT);
+}
+
+void loop() {
+  int val = analogRead(Pot_Input); 
+  int angle = map(val,1,1023,0,270);
+  Serial.println(angle);
+}
