8accb6a78901bda21f85eb8e87a65438acce3c2b
BenMckay6 <benjamin6mckay@gmail.com>
2021-11-03 14:19:38 -0300

    Pinout changes

--- src4/motor_trialEMGV2.ino
+++ src4/motor_trialEMGV2.ino
@@ -5,29 +5,32 @@
 // Pins for all inputs, keep in mind the PWM defines must be on PWM pins
 // the default pins listed are the ones used on the Redbot (ROB-12097) with
 // the exception of STBY which the Redbot controls with a physical switch
-#define AIN1 2
-#define BIN1 7
-#define AIN2 4
-#define BIN2 8
-#define PWMA 5
-#define PWMB 6
-#define STBY 9
+
+#define DIR1 17 // YELLOW DIR1 ON MOTOR DRIVER
+#define DIR2 16 // WHITE 
+#define PWM1 22
+
+#define EMG 23
 
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
 const int offsetA = 1;
-const int offsetB = 1;
 
 // Initializing motors.  The library will allow you to initialize as many
 // motors as you have memory for.  If you are using functions like forward
 // that take 2 motors as arguements you can either write new functions or
 // call the function more than once.
-Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
-Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+Motor motor1 = Motor(DIR1, DIR2, PWM1, offsetA);
 
 void setup()
 {
- //Nothing here
+Serial.begin(9600);
+  pinMode(DIR1,OUTPUT);
+  pinMode(DIR2,OUTPUT);
+
+  pinMode(PWM1,OUTPUT);
+  pinMode(EMG,INPUT);
+  
 }
 
 
@@ -42,42 +45,38 @@ void loop()
    motor1.brake();
    delay(1000);
    
-   //Use of the drive function which takes as arguements the speed
-   //and optional duration.  A negative speed will cause it to go
-   //backwards.  Speed can be from -255 to 255.  Also use of the 
-   //brake function which takes no arguements.
-   motor2.drive(255,1000);
-   motor2.drive(-255,1000);
-   motor2.brake();
-   delay(1000);
-   
+   int val = analogRead(EMG);
+   int angle = map(val,1,1023,0,180);
+   int target = angle;
+   Serial.println(angle);
+   //Reading emg value
+
    //Use of the forward function, which takes as arguements two motors
    //and optionally a speed.  If a negative number is used for speed
    //it will go backwards
-   forward(motor1, motor2, 150);
+   forward(motor1, 150);
    delay(1000);
    
    //Use of the back function, which takes as arguments two motors 
    //and optionally a speed.  Either a positive number or a negative
    //number for speed will cause it to go backwards
-   back(motor1, motor2, -150);
+   back(motor1, -150);
    delay(1000);
    
    //Use of the brake function which takes as arguments two motors.
    //Note that functions do not stop motors on their own.
-   brake(motor1, motor2);
+   brake(motor1);
    delay(1000);
    
    //Use of the left and right functions which take as arguements two
    //motors and a speed.  This function turns both motors to move in 
    //the appropriate direction.  For turning a single motor use drive.
-   left(motor1, motor2, 100);
-   delay(1000);
-   right(motor1, motor2, 100);
+   drive(motor1, 100);
    delay(1000);
    
+   
    //Use of brake again.
-   brake(motor1, motor2);
+   brake(motor1);
    delay(1000);
    
 }
