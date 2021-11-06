d741a40f0fc24cf5b01279f88e0432e664428153
Jen Vibert <jennifer.vibert@dal.ca>
2021-11-06 16:37:07 -0300

    Moved three files to src

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+
+#include "SparkFun_TB6612.h"
+#include <Arduino.h>
+
+
+
+#define DIR1 17 // YELLOW DIR1 ON MOTOR DRIVER
+#define PWM1 13
+#define EMG 23
+#define DIR2 16
+
+const int OffsetA = 1;
+Motor motor1 = Motor(DIR1,DIR2,PWM1,OffsetA);
+Motor motor2 = Motor(DIR1,DIR2,PWM1,OffsetA);
+
+void setup()
+{
+  pinMode(EMG,INPUT);
+  Serial.begin(9600);
+}
+ 
+ 
+void loop()
+{
+   //Use of the drive function which takes as arguements the speed
+   //and optional duration.  A negative speed will cause it to go
+   //backwards.  Speed can be from -255 to 255.  Also use of the 
+   //brake function which takes no arguements.
+   motor1.drive(255,1000);
+   motor1.drive(-255,1000);
+   motor1.brake();
+   delay(1000);
+   
+   int val = analogRead(EMG);
+   int angle = map(val,1,1023,0,180);
+   Serial.println(angle);
+   //Reading EMG value
+
+   //Use of the forward function, which takes as arguements two motors
+   //and optionally a speed.  If a negative number is used for speed
+   //it will go backwards
+   forward(motor1,motor2, 150);
+   delay(1000);
+   
+   //Use of the back function, which takes as arguments two motors 
+   //and optionally a speed.  Either a positive number or a negative
+   //number for speed will cause it to go backwards
+   back(motor1,motor2, -150);
+   delay(1000);
+   
+   //Use of the brake function which takes as arguments two motors.
+   //Note that functions do not stop motors on their own.
+   brake(motor1, motor2);
+   delay(1000);
+   
+   //Use of the left and right functions which take as arguements two
+   //motors and a speed.  This function turns both motors to move in 
+   //the appropriate direction.  For turning a single motor use drive.
+   motor1.drive(100);
+   delay(1000);
+   
+   //Use of brake again.
+   brake(motor1,motor2);
+   delay(1000);
+   
+}
