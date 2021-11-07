94bca619a5d3069e985b91b2b10bb0ed7443f476
dmadariaga4 <84297351+dmadariaga4@users.noreply.github.com>
2021-11-07 12:08:47 -0400

    Multiple changes

--- src/main.cpp
+++ src/main.cpp
@@ -27,43 +27,63 @@ void loop()
    //and optional duration.  A negative speed will cause it to go
    //backwards.  Speed can be from -255 to 255.  Also use of the 
    //brake function which takes no arguements.
-   motor1.drive(255,1000);
-   motor1.drive(-255,1000);
-   motor1.brake();
-   delay(1000);
-   
+  //  motor1.drive(255,1000);
+  //  motor1.drive(-255,1000);
+  //  motor1.brake();
+  //  delay(1000);
+
+      motor1.drive(40,1000);
+
+
    int val = analogRead(EMG);
-   int angle = map(val,1,1023,0,180);
+   int angle = map(val,1,1023,0,270);
    Serial.println(angle);
    //Reading EMG value
 
     PrevPos = DesPos;
     DesPos = angle;
-    
- while (PrevPos = 0){
-   forward(motor1,motor2, 10);
-   delay(1000);
-   PrevPos++;
- }
 
- while (DesPos > PrevPos){
-   forward(motor1,motor2, 10);
-   delay(1000);
-   PrevPos++;
- }
+   
+  while (PrevPos = 0){
+    Serial.println("In first Loop");
+    forward(motor1,motor2, 30);
+    delay(1000);
+    PrevPos++;
+    val = analogRead(EMG);
+    angle = map(val,1,1023,0,270);
+    Serial.println(angle);
+  }
 
- while (DesPos < PrevPos){
-   back(motor1,motor2, -10);
-   delay(1000);
-   DesPos++;
- }
+  while (DesPos > PrevPos){
+    Serial.println("In Second Loop");
+    motor1.drive(40,1000);
+    //forward(motor1,motor2,40);
+    delay(1000);
+    //forward(motor1,motor2,30);
+    PrevPos++;
+    val = analogRead(EMG);
+    angle = map(val,1,1023,0,270);
+    Serial.println(angle);
+  }
 
- while (DesPos = PrevPos){
-   brake(motor1, motor2);
-   delay(1000);
- break;
- }
+  while (DesPos < PrevPos){
+    Serial.println("In Third Loop");
+    motor1.drive(-30, 1000);
+    //back(motor1,motor2, -20);
+    delay(1000);
+    DesPos++;
+    val = analogRead(EMG);
+    angle = map(val,1,1023,0,270);
+    Serial.println(angle);
+  }
 
-  
-
+  while (DesPos = PrevPos){
+    Serial.println("In Break Loop");
+    brake(motor1, motor2);
+    delay(1000);
+    val = analogRead(EMG);
+    angle = map(val,1,1023,0,270);
+    Serial.println(angle);
+  break;
+  }
 }
